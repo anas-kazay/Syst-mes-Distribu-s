5a739cd6ba3b2dd38899378b2b445fb06bd09c20
anas-kazay <anaskazay@gmail.com>
2024-10-14 01:34:08 +0100

    add real time analytics with kafka

--- demo-spring-cloud-kafka/src/main/java/com/example/demospringcloudkafka/services/PageEventService.java
+++ demo-spring-cloud-kafka/src/main/java/com/example/demospringcloudkafka/services/PageEventService.java
@@ -1,11 +1,18 @@
 package com.example.demospringcloudkafka.services;
 
 import com.example.demospringcloudkafka.entities.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -27,7 +34,7 @@ public class PageEventService {
     @Bean
     public Supplier<PageEvent> pageEventSupplier() {
         return () -> {
-            PageEvent pageEvent = new PageEvent("pageEvent", "anas", new java.util.Date(), new Random().nextInt(9000));
+            PageEvent pageEvent = new PageEvent(Math.random()>0.5?"P1":"P2", "anas", new java.util.Date(), new Random().nextInt(9000));
             logger.info("PageEvent generated: " + pageEvent);
             return pageEvent;
         };
@@ -40,5 +47,20 @@ public class PageEventService {
             logger.info("PageEvent processed: " + pageEvent);
             return pageEvent;
         };
+    }
+
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction() {
+        return (kStream) -> {
+            return kStream.filter((k,v)->v.getDuration()>10)
+                    .map((k,v)->new KeyValue<>(v.getName(),0L))
+                    .groupBy((k,v)->k, Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v))
+                    ;
+        };
+
     }
 }
--- demo-spring-cloud-kafka/src/main/java/com/example/demospringcloudkafka/web/PageEventRestController.java
+++ demo-spring-cloud-kafka/src/main/java/com/example/demospringcloudkafka/web/PageEventRestController.java
@@ -1,21 +1,38 @@
 package com.example.demospringcloudkafka.web;
 
 import com.example.demospringcloudkafka.entities.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
 public class PageEventRestController {
 
     private StreamBridge streamBridge;
+    private InteractiveQueryService interactiveQueryService;
 
-    public PageEventRestController(StreamBridge streamBridge) {
+    public PageEventRestController(
+            StreamBridge streamBridge,
+            InteractiveQueryService interactiveQueryService
+    ) {
         this.streamBridge = streamBridge;
+        this.interactiveQueryService = interactiveQueryService;
     }
 
     @GetMapping("/publish/{topic}/{name}")
@@ -23,5 +40,22 @@ public class PageEventRestController {
         PageEvent pageEvent = new PageEvent(name ,Math.random()>0.5 ? "anas":"yassin" , new Date(), new Random().nextInt(1000));
         streamBridge.send(topic, pageEvent);
         return pageEvent;
+    }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String,Long>> analytics(){
+        return Flux.interval(Duration.ofSeconds(2))
+                .map(sequence->{
+                    Map<String,Long> stringLongMap = new HashMap<>();
+                    ReadOnlyWindowStore<String,Long> windowStore = interactiveQueryService.getQueryableStore("page-count", QueryableStoreTypes.windowStore());
+                    Instant now = Instant.now();
+                    Instant from = now.minusSeconds(5000);
+                    KeyValueIterator<Windowed<String>,Long> fetchAll = windowStore.fetchAll(from, now);
+                    while(fetchAll.hasNext()){
+                        KeyValue<Windowed<String>,Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                }).share();
     }
 }
